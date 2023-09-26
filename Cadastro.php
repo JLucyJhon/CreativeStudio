20e2e2d75b820108fb475e3118ce0017c7311fbf
PauloVictor007 <143506800+PauloVictor007@users.noreply.github.com>
2023-09-26 08:48:11 -0300

    FDS O DRAKE E TRAVIS

--- Cadastro.php
+++ Cadastro.php
@@ -1,1 +1,9 @@
 <?php
+$cadastro = ['nome'=>'Paulo Victor',
+'idade'=>18,
+'sexo'=>'M',
+'telefone'=>'(91)40028922'];
+
+echo "Me chamo ".$cadastro["nome"]." tenho ".$cadastro["idade"]." sou do sexo ".$cadastro["sexo"]." e meu telefone de contato é ".$cadastro["telefone"];
+
+?>
